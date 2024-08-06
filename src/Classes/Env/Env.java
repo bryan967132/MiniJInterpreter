9c1386e3ff6182eb5f82a2f2d20aadb2a2c28acc
bryan967132 <bryan.tejaxun@gmail.com>
2024-08-05 18:13:50 -0600

    [Update] Env

--- src/Classes/Env/Env.java
+++ src/Classes/Env/Env.java
@@ -1,10 +1,12 @@
 package Classes.Env;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 import Classes.Instructions.Function;
 import Classes.Utils.ReturnType;
 import Classes.Utils.Type;
 import Classes.Utils.TypeError;
+import Classes.Utils.DataType;
 import Classes.Utils.Error;
 import Classes.Utils.Outs;
 public class Env {
@@ -16,9 +18,9 @@ public class Env {
         this.previous = previous;
         this.name = name;
     }
-    public boolean saveID(String id, Object value, Type type, int line, int column) {
+    public boolean saveID(String id, ReturnType value, DataType type, int line, int column) {
         if(!this.ids.containsKey(id)) {
-            this.ids.put(id, new Symbol(value, id, type, null));
+            this.ids.put(id, new Symbol(value, id, type));
             return true;
         }
         setError("Redeclaración de variable existente", line, column);
@@ -35,15 +37,94 @@ public class Env {
         setError("Acceso a variable inexistente. '" + id +"'", line, column);
         return null;
     }
+    public Symbol getValueID(String id, ArrayList<Integer[]> pos, int line, int column) {
+        Env current = this;
+        while(current != null) {
+            if(current.ids.containsKey(id)) {
+                Symbol mySymbol = current.ids.get(id);
+                if(mySymbol.value.type1 == Type.VECTOR) {
+                    if(mySymbol.value.dimensions >= pos.size()) {
+                        ReturnType r = mySymbol.value.getPos(current, id, pos);
+                        if(r != null) {
+                            return new Symbol(r, id, mySymbol.value.getDataType());
+                        }
+                        return null;
+                    }
+                    setError("Acceso por índices incorrecto, más de los necesarios. '" + id +"'", line, column);
+                    return null;
+                }
+                setError("Acceso por índices a una variable de tipo diferente a Vector. '" + id +"'", line, column);
+                return null;
+            }
+            current = current.previous;
+        }
+        setError("Acceso a variable inexistente. '" + id +"'", line, column);
+        return null;
+    }
     public boolean reasignID(String id, ReturnType value, int line, int column) {
         Env current = this;
         while(current != null) {
             if(current.ids.containsKey(id)) {
-                if(current.ids.get(id).type == value.type || current.ids.get(id).type == Type.DOUBLE && value.type == Type.INT) {
-                    current.ids.get(id).value = value.value;
+                Symbol mySymbol = current.ids.get(id);
+                if(mySymbol.type.type1 != Type.VECTOR && (mySymbol.type.type1 == value.type1 || mySymbol.type.type1 == Type.DOUBLE && value.type1 == Type.INT)) {
+                    mySymbol.value = value.getClone();
                     return true;
                 }
-                setError("Los tipos no coinciden en la asignación. Intenta asignar un \"" + value.type.getValue() + "\" a un \"" + current.ids.get(id).type.getValue() + "\"", line, column);
+                if(mySymbol.type.type1 == Type.VECTOR && mySymbol.type.type1 == value.type1 && (mySymbol.type.type2 == value.type2 || mySymbol.type.type2 == Type.DOUBLE && value.type2 == Type.INT) && mySymbol.value.dimensions == value.dimensions) {
+                    mySymbol.value = value.getClone();
+                    if(mySymbol.type.type2 == Type.DOUBLE && value.type2 == Type.INT) {
+                        value.intToDouble();
+                    }
+                    return true;
+                }
+                if(value.type1 != Type.VECTOR) {
+                    setError("Los tipos no coinciden en la asignación. Intenta asignar un '" + value.type1.getValue() + "' a un '" + mySymbol.type.type1.getValue() + "'", line, column);
+                } else if(!(value.type2 == mySymbol.type.type2 || mySymbol.type.type2 == Type.DOUBLE && value.type2 == Type.INT)) {
+                    setError("Los tipos no coinciden en la asignación. Intenta asignar un '" + value.type2.getValue() + "' a un '" + mySymbol.type.type2.getValue() + "'", line, column);
+                } else {
+                    setError("Las dimensiones del vector no es la esperada", line, column);
+                }
+                return false;
+            }
+            current = current.previous;
+        }
+        setError("Resignación de valor a variable inexistente", line, column);
+        return false;
+    }
+    public boolean reasignID(String id, ArrayList<Integer[]> pos, ReturnType value, int line, int column) {
+        Env current = this;
+        while(current != null) {
+            if(current.ids.containsKey(id)) {
+                Symbol mySymbol = current.ids.get(id);
+                if(mySymbol.value.type1 == Type.VECTOR) {
+                    if(mySymbol.value.dimensions >= pos.size()) {
+                        ReturnType r = mySymbol.value.getPos(current, id, pos);
+                        if(r != null) {
+                            if(r.type1 != Type.VECTOR && (r.type1 == value.type1 || r.type1 == Type.DOUBLE && value.type1 == Type.INT)) {
+                                r.value = value.value;
+                                return true;
+                            }
+                            if(r.type1 == Type.VECTOR && r.type1 == value.type1 && (r.type2 == value.type2 || r.type2 == Type.DOUBLE && value.type2 == Type.INT) && r.dimensions == value.dimensions) {
+                                if(r.type2 == Type.DOUBLE && value.type2 == Type.INT) {
+                                    value.intToDouble();
+                                }
+                                r.value = value.value;
+                                return true;
+                            }
+                            if(value.type1 != Type.VECTOR) {
+                                setError("Los tipos no coinciden en la asignación. Intenta asignar un '" + value.type1.getValue() + "' a un '" + r.type1.getValue() + "'", line, column);
+                            } else if(!(value.type2 == r.type2 || r.type2 == Type.DOUBLE && value.type2 == Type.INT)) {
+                                setError("Los tipos no coinciden en la asignación. Intenta asignar un '" + value.type2.getValue() + "' a un '" + r.type2.getValue() + "'", line, column);
+                            } else {
+                                setError("Las dimensiones del vector no es la esperada", line, column);
+                            }
+                        }
+                        return false;
+                    }
+                    setError("Reasignación por índices incorrecto, más de los necesarios. '" + id +"'", line, column);
+                    return false;
+                }
+                setError("Reasignación por índices a una variable de tipo diferente a Vector. '" + id +"'", line, column);
                 return false;
             }
             current = current.previous;
@@ -67,7 +148,7 @@ public class Env {
             }
             current = current.previous;
         }
-        setError("Acceso a función inexistente", line, column);
+        setError("Acceso a función inexistente '" + id + "'", line, column);
         return null;
     }
     public Env getGlobal() {
--- src/Classes/Env/Symbol.java
+++ src/Classes/Env/Symbol.java
@@ -1,14 +1,13 @@
 package Classes.Env;
-import Classes.Utils.Type;
+import Classes.Utils.DataType;
+import Classes.Utils.ReturnType;
 public class Symbol {
-    public Object value;
-    String id;
-    public Type type;
-    Type arrayType;
-    public Symbol(Object value, String id, Type type, Type arrayType) {
+    public ReturnType value;
+    public String id;
+    public DataType type;
+    public Symbol(ReturnType value, String id, DataType type) {
         this.value = value;
         this.id = id;
         this.type = type;
-        this.arrayType = arrayType;
     }
 }
